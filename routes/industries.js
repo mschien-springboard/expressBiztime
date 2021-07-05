ea4759d289f9e74673638300fef54cdb039cbe13
Michael Schienbein <moomba0@gmail.com>
2021-07-05 16:16:09 +0700

    add tests

--- routes/industries.js
+++ routes/industries.js
@@ -62,52 +62,52 @@ router.get('/:code', async (req, res, next) => {
 });
 
 
-router.post('/', async (req, res, next) => {
-    try {
-        const { code, name, description } = req.body;
-        if (!code || !name || !description) {
-            throw new ExpressError("Missing required data", 400);
-        };
-        const results = await db.query(
-            `INSERT INTO companies (code, name, description) 
-        VALUES ($1, $2, $3) 
-        RETURNING code, name, description`, [code, name, description]);
-        return res.status(201).json({ company: results.rows[0] });
-    } catch (e) {
-        return next(e);
-    };
-});
+// router.post('/', async (req, res, next) => {
+//     try {
+//         const { code, name, description } = req.body;
+//         if (!code || !name || !description) {
+//             throw new ExpressError("Missing required data", 400);
+//         };
+//         const results = await db.query(
+//             `INSERT INTO companies (code, name, description) 
+//         VALUES ($1, $2, $3) 
+//         RETURNING code, name, description`, [code, name, description]);
+//         return res.status(201).json({ company: results.rows[0] });
+//     } catch (e) {
+//         return next(e);
+//     };
+// });
 
-router.put('/:code', async (req, res, next) => {
-    try {
-        const { code } = req.params;
-        const { name, description } = req.body;
-        if (!name || !description) {
-            throw new ExpressError("Missing required data", 400);
-        };
-        const results = await db.query(
-            `UPDATE companies SET code=$1, name=$2, description=$3 
-            WHERE code=$1 RETURNING code, name, description`, [code, name, description]);
-        if (results.rows.length === 0) {
-            throw new ExpressError(`Company Code: ${code} not found`, 404);
-        };
-        return res.send({ company: results.rows[0] });
-    } catch (e) {
-        return next(e);
-    };
-});
+// router.put('/:code', async (req, res, next) => {
+//     try {
+//         const { code } = req.params;
+//         const { name, description } = req.body;
+//         if (!name || !description) {
+//             throw new ExpressError("Missing required data", 400);
+//         };
+//         const results = await db.query(
+//             `UPDATE companies SET code=$1, name=$2, description=$3 
+//             WHERE code=$1 RETURNING code, name, description`, [code, name, description]);
+//         if (results.rows.length === 0) {
+//             throw new ExpressError(`Company Code: ${code} not found`, 404);
+//         };
+//         return res.send({ company: results.rows[0] });
+//     } catch (e) {
+//         return next(e);
+//     };
+// });
 
-router.delete('/:code', async (req, res, next) => {
-    try {
-        const { code } = req.params;
-        const results = await db.query(`DELETE FROM companies WHERE code=$1 RETURNING code`, [code]);
-        if (results.rowCount === 0) {
-            throw new ExpressError(`Company Code: ${code} not found`, 404);
-        };
-        return res.send({ msg: `Deleted Company: ${code}` });
-    } catch (e) {
-        return next(e);
-    };
-});
+// router.delete('/:code', async (req, res, next) => {
+//     try {
+//         const { code } = req.params;
+//         const results = await db.query(`DELETE FROM companies WHERE code=$1 RETURNING code`, [code]);
+//         if (results.rowCount === 0) {
+//             throw new ExpressError(`Company Code: ${code} not found`, 404);
+//         };
+//         return res.send({ msg: `Deleted Company: ${code}` });
+//     } catch (e) {
+//         return next(e);
+//     };
+// });
 
 module.exports = router;
